ce79ca3386787e04b6d6b4a04d0a0defc5aed77c
Naman Garg <namangarg155@gmail.com>
2021-04-22 13:57:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.justjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        /*display(quantity);
+        displayPrice(quantity * 5);*/
+        int price = quantity * 5;
+        String message = "Total: $" + price;
+        message = message + "\nThank You!";
+        displayMessage(message);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    /*
+    * This method displays the quantity
+    * */
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void decrement(View view) {
+        quantity -= 1;
+        display(quantity);
+        //displayPrice(quantity*5);
+    }
+
+    public void increment(View view) {
+        quantity += 1;
+        display(quantity);
+        //displayPrice(quantity*5);
+    }
+
+}
